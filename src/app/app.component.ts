0a9505ae8b20b622f914f9e77e31ddcb3f4c18ae
homoantropos <homoantropos@gmail.com>
2020-03-26 15:43:19 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { EventCreatorComponent } from './event-creator/event-creator.component';
+import { Event } from './single-event/event.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+calendar: Event[];
+isVisible = false;
+search = '';
+searchField = 'title';
+@ViewChild('filterInput') filterInputRef: ElementRef;
+
   title = 'calendar2';
+	constructor(){
+		this.calendar = [
+		new Event ('Міжнародний спортивно-масовий захід серед учнів "ISF WORLD COOL GAMES"', '18.11.20', '23.11.20', 6, 'Київ', 'Україна', 'Київ'),
+		new Event ('Чемпіонат України з рукопашного бою серед учнів', '15.03.20', '17.03.20', 3, 'Київ', 'Україна', 'Київ'),
+		new Event ('ІІ зимова Гімназіада України з лижних гонок', '25.03.20', '26.03.20', 2, 'смт Тисовець', 'Україна', 'Львівська обл.'),
+		new Event ('XV літня Універсіада України з тхеквондо (ВТФ) фінал', '05.03.20', '07.03.20', 3, 'Івано-Франківськ', 'Україна', 'Івано-Франківська обл.'),
+		new Event ('Всесвітня літня Гімназіада', '10.10.20', '27.10.20', 17, 'Дзіндзян', 'Китайська Народна Республіка', ''),
+		new Event ('Європейські університетські ігри', '18.07.20', '23.07.20', 12, 'Бєлград', 'Сербія', ''),
+		new Event ('Всеукраїнський спортивно-масовий захід серед учнів "COOL GAMES" 2020-2021 років', '17.09.20', '23.02.21', 120, 'регіони', 'Україна', 'всі регіони'),
+		new Event ('Чемпіонат світу з вейкбордингу та воднолижного спорту серед студентів', '22.09.20', '27.09.20', 6, 'Дніпро', 'Україна', 'Дніпропетровська обл.'),
+		new Event ('Кубок України з боксу серед студентів', '18.04.20', '20.04.20', 3, 'Полтава', 'Україна', 'Полтавська обл.'),
+		];
+	}
+
+	addEvent(event: Event) {
+		this.calendar.unshift(event);
+	}
+
+	chandgeSearchField() {
+		if(this.searchField == 'title'){
+			this.searchField = 'start';
+		} else {
+			this.searchField = 'title';
+		}
+		this.filterInputRef.nativeElement.focus();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
+import { SingleEventComponent } from './single-event/single-event.component';
+import { EventCreatorComponent } from './event-creator/event-creator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToggleDirective } from './directives/toggle.directive';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleEventComponent,
+    EventCreatorComponent,
+    ToggleDirective,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
