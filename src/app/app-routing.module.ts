f97fc0d23fcfb04313ecdeb92117dd3cc9d91df0
Sergii <homoantropos@gmail.com>
2020-04-22 21:16:43 +0300

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home/home.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AuthGuard} from './auth-guard/auth.guard';
 
 const routes: Routes = [
   {path: 'schedule',
-    loadChildren: () => import('./schedule/schedule.module').then(m => m.ScheduleModule)},
+    loadChildren: () => import('./schedule/schedule.module').then(m => m.ScheduleModule),
+  canActivate: [AuthGuard]},
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {AuthService} from './auth-guard/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,10 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-
   title = 'calendar2';
-
-  constructor() {	}
+  constructor(
+    public auth: AuthService
+  ) {
+  }
 
 }
